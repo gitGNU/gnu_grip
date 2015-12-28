03398d58cb849a9afe0fa9861c59c174837705c8
David Pirotte <david@altosw.be>
2015-12-28 21:59:20 -0200

    adding hex colour coding support and tango palette
    * grip/clutter/colour.scm:  Adding xcolour?, xcolour->int, xcolour->rgb,
      xcolour->rgba.
    
      Adding colours from the tango palette http://tango.freedesktop.org

--- grip/clutter/colour.scm
+++ grip/clutter/colour.scm
@@ -35,17 +35,61 @@
 
 
 (define-module (grip clutter colour)
+  #:use-module (ice-9 regex)
   #:use-module (srfi srfi-1)
   #:use-module (oop goops)
   #:use-module (gnome-2)
   #:use-module (gnome gobject)
   #:use-module (gnome clutter)
+  #:use-module (grip utils)
+  #:use-module (grip strings)
+  #:use-module (grip lists)
 
   #:export (get-colour
 	    get-darker-colour
 	    get-lighter-colour
 	    get-rgba-r get-rgba-g get-rgba-b get-rgba-a
-	    get-rgba-values))
+	    get-rgba-values
+
+	    xcolour?
+	    xcolour->int
+	    xcolour->rgb
+	    xcolour->rgba
+
+	    %tango-butter
+	    %tango-butter-light
+	    %tango-butter-dark
+
+	    %tango-orange
+	    %tango-orange-light
+	    %tango-orange-dark
+
+	    %tango-chocolate
+	    %tango-chocolate-light
+	    %tango-chocolate-dark
+
+	    %tango-chameleon
+	    %tango-chameleon-light
+	    %tango-chameleon-dark
+
+	    %tango-sky-blue
+	    %tango-sky-blue-light
+	    %tango-sky-blue-dark
+
+	    %tango-plum
+	    %tango-plum-light
+	    %tango-plum-dark
+
+	    %tango-scarlet-red
+	    %tango-scarlet-red-light
+	    %tango-scarlet-red-dark
+
+	    %tango-aluminium-1
+	    %tango-aluminium-2
+	    %tango-aluminium-3
+	    %tango-aluminium-4
+	    %tango-aluminium-5
+	    %tango-aluminium-6))
 
 
 (define* (get-colour colour #:key (alpha 255) (fallback "Chocolate"))
@@ -104,3 +148,100 @@
 	  (get-rgba-g colour #:float float)
 	  (get-rgba-b colour #:float float)
 	  (get-rgba-a colour #:float float)))
+
+
+;;;
+;;; Xcolour [hex code] procs
+;;;
+
+;; Let's keep these for the record, but I finally discovered that
+;; clutter-color-from-string does the same job [and more]! Oh well ...
+
+(define %xcolour-regexp-1
+  (make-regexp "^#[0-9a-fA-F]{3}$"))
+
+(define %xcolour-regexp-2
+  (make-regexp "^#[0-9a-fA-F]{6}$"))
+
+(define (xcolour? xcolour)
+  ;; ex: "#fff", "#ffffff"
+  (let* ((xlist (drop (string->list xcolour) 1))
+	 (xlist-6 (cond ((regexp-exec %xcolour-regexp-1 xcolour)
+			 (interleave xlist xlist))
+			((regexp-exec %xcolour-regexp-2 xcolour)
+			 xlist)
+			(else
+			 #f))))
+    (and xlist-6
+	 (str/read (string-append "#x"
+				  (list->string xlist-6))))))
+
+(define (xcolour->int xcolour channel)
+  "Return the integer value of an 8-bit color channel for XCOLOUR."
+  (let* ((xcolour (if (integer? xcolour)
+		     xcolour
+		     (xcolour? xcolour)))
+	 (offset (case channel
+		   ((alpha) 24)
+		   ((red) 16)
+		   ((green) 8)
+		   ((blue) 0)))
+	 (mask (ash #xff offset)))
+    (ash (logand mask xcolour)
+	 (- offset))))
+
+(define (xcolour->rgb xcolour)
+  "Returns a list of red, green and blue integer values for XCOLOUR."
+  (let ((xcolour (if (integer? xcolour)
+		     xcolour
+		     (xcolour? xcolour))))
+    (list (xcolour->int xcolour 'red)
+	  (xcolour->int xcolour 'green)
+	  (xcolour->int xcolour 'blue))))
+
+(define* (xcolour->rgba xcolour #:key (alpha 255))
+  "Returns a list of red, green, blue and alpha integer values for
+XCOLOUR. The default value for the optional alpha channel keyword is 255."
+  (append (xcolour->rgb xcolour)
+	  (list alpha)))
+
+
+;;;
+;;; Tango color pallete
+;;;   http://tango.freedesktop.org
+;;;
+
+(define %tango-butter (xcolour->rgba "#edd400"))
+(define %tango-butter-light (xcolour->rgba "#fce94f"))
+(define %tango-butter-dark (xcolour->rgba "#c4a000"))
+
+(define %tango-orange (xcolour->rgba "#f57900"))
+(define %tango-orange-light (xcolour->rgba "#fcaf3e"))
+(define %tango-orange-dark (xcolour->rgba "#ce5c00"))
+
+(define %tango-chocolate (xcolour->rgba "#c17d11"))
+(define %tango-chocolate-light (xcolour->rgba "#e9b96e"))
+(define %tango-chocolate-dark (xcolour->rgba "#8f5902"))
+
+(define %tango-chameleon (xcolour->rgba "#73d216"))
+(define %tango-chameleon-light (xcolour->rgba "#8ae234"))
+(define %tango-chameleon-dark (xcolour->rgba "#4e9a06"))
+
+(define %tango-sky-blue (xcolour->rgba "#3465a4"))
+(define %tango-sky-blue-light (xcolour->rgba "#729fcf"))
+(define %tango-sky-blue-dark (xcolour->rgba "#204a87"))
+
+(define %tango-plum (xcolour->rgba "#75507b"))
+(define %tango-plum-light (xcolour->rgba "#ad7fa8"))
+(define %tango-plum-dark (xcolour->rgba "#5c3566"))
+
+(define %tango-scarlet-red (xcolour->rgba "#cc0000"))
+(define %tango-scarlet-red-light (xcolour->rgba "#ef2929"))
+(define %tango-scarlet-red-dark (xcolour->rgba "#a40000"))
+
+(define %tango-aluminium-1 (xcolour->rgba "#eeeeec"))
+(define %tango-aluminium-2 (xcolour->rgba "#d3d7cf"))
+(define %tango-aluminium-3 (xcolour->rgba "#babdb6"))
+(define %tango-aluminium-4 (xcolour->rgba "#888a85"))
+(define %tango-aluminium-5 (xcolour->rgba "#555753"))
+(define %tango-aluminium-6 (xcolour->rgba "#2e3436"))
