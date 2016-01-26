e4b31dc41e00892c14b37ea30f2ff5ac63d9387a
David Pirotte <david@altosw.be>
2016-01-26 21:58:14 -0200

    bumping copyright years range
    * grip/clutter.scm:
    * grip/clutter/actor.scm:
    * grip/clutter/align.scm:
    * grip/clutter/animation.scm:
    * grip/clutter/apple.scm:
    * grip/clutter/bin.scm:
    * grip/clutter/bouncer.scm:
    * grip/clutter/box.scm:
    * grip/clutter/clock.scm:
    * grip/clutter/colour.scm:
    * grip/clutter/constraint.scm:
    * grip/clutter/drag.scm:
    * grip/clutter/drop.scm:
    * grip/clutter/examples/bouncer.scm:
    * grip/clutter/examples/drag.scm:
    * grip/clutter/examples/drop.scm:
    * grip/clutter/examples/grid.scm:
    * grip/clutter/examples/scroll.scm:
    * grip/clutter/examples/spline.scm:
    * grip/clutter/examples/toolbar.scm:
    * grip/clutter/examples/utils.scm:
    * grip/clutter/globals.scm:
    * grip/clutter/gravity.scm:
    * grip/clutter/grid.scm:
    * grip/clutter/image.scm:
    * grip/clutter/label.scm:
    * grip/clutter/orientation.scm:
    * grip/clutter/stage.scm:
    * grip/clutter/text.scm:
    * grip/clutter/toolbar-item.scm:
    * grip/clutter/toolbar.scm:
    * grip/clutter/tooltip.scm:
    * grip/clutter/utils.scm:
    * grip/config.scm:
    * grip/dates.scm:
    * grip/db/filters.scm:
    * grip/db/sqlite.scm:
    * grip/do.scm:
    * grip/fs-ops.scm:
    * grip/g-export.scm:
    * grip/gnome.scm:
    * grip/gnome/colours.scm:
    * grip/gnome/filechooser.scm:
    * grip/gnome/gtk2.scm:
    * grip/gnome/key-press.scm:
    * grip/gnome/mdialog-1b.scm:
    * grip/gnome/mdialog-2b.scm:
    * grip/gnome/mdialogs.scm:
    * grip/goops.scm:
    * grip/i18n.scm:
    * grip/keyword.scm:
    * grip/lists.scm:
    * grip/locale.scm:
    * grip/m4/support.scm:
    * grip/mmfdi.scm:
    * grip/nbs.scm:
    * grip/nbs/fp.scm:
    * grip/nbs/hex.scm:
    * grip/nbs/nb2str.scm:
    * grip/nbs/pi.scm:
    * grip/nbs/str2nb.scm:
    * grip/passwd.scm:
    * grip/push.scm:
    * grip/reexport.scm:
    * grip/regexp.scm:
    * grip/server.scm:
    * grip/sqlite3.scm:
    * grip/strings.scm:
    * grip/support/slib.scm:
    * grip/swap.scm:
    * grip/tex-utils.scm:
    * grip/utils.scm:
    * grip/xft.scm:  use Copyright (C) 2011 - 2016

--- grip/gnome.scm
+++ grip/gnome.scm
@@ -1,7 +1,7 @@
 ;; -*- mode: scheme; coding: utf-8 -*-
 
 ;;;;
-;;;; Copyright (C) 2011 - 2015
+;;;; Copyright (C) 2011 - 2016
 ;;;; David Pirotte <david at altosw dot be>
 
 ;;;; This file is part of Grip.
