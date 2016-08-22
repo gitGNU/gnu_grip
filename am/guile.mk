e308c8d0abd8b2253286c50900d42aa8ff250ec9
David Pirotte <david@altosw.be>
2016-08-22 00:10:19 -0300

    Prepare for Guile-2.2 - continued
    * Makefile.am:
    * am/guile.mk:
    * configure.ac:  Use the default guild version.
    
    * meta/guild.in:  Deleted.

--- am/guile.mk
+++ am/guile.mk
@@ -44,7 +44,7 @@ GUILE_WARNINGS = -Wunbound-variable -Warity-mismatch -Wformat
 SUFFIXES = .scm .go
 .scm.go:
 	$(AM_V_GEN)$(top_builddir)/env \
-	$(top_builddir)/meta/guild compile $(GUILE_WARNINGS) -o "$@" "$<"
+	guild compile $(GUILE_WARNINGS) -o "$@" "$<"
 
 printenv:
 	printf '$(gev)\n$(gglobalsitedir)\n$(gsiteccache)\n'
